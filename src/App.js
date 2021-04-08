6cc06b4287d392d0f30a853dc834afd4f4c56c1d
Alex Bartley Nees <alexbartleynees@gmail.com>
2021-04-08 18:31:42 +1200

    Finish

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react'
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
+
+  const [showAddTask, setShowAddTask] = useState(false)
 
   const [tasks, setTasks] = useState([
     {
@@ -20,10 +23,31 @@ function App() {
     }
   ])
 
+  //Add Task
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) +1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
+  //Delete Task
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter ((task) => task.id !== id))
+  }
+
+  //Toggle Reminder
+
+  let toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
+
   return (
     <div className="container">
-      <Header />
-      <Tasks tasks={tasks} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask  onAdd={addTask}/>}
+     {tasks.length > 0 ? <Tasks tasks={tasks} onToggle={toggleReminder} onDelete={deleteTask} /> : 'No Tasks To Show'}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,13 @@
 import Button from './Button'
 
 
-const Header = ({title}) => {
+const Header = ({title, onAdd, showAdd}) => {
 
-    const onClick = () => {
-        console.log('Click')
-    }
 
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Hello' onClick={onClick} />
+            <Button color={showAdd ? 'red': 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
             
         </header>
     )
